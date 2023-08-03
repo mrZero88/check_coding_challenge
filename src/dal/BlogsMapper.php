4fbfdfea97fda11ae1dd3fd4f6ec18f4c7b65899
Daniel <danielcorreia1988@outlook.com>
2023-08-03 20:23:19 +0200

    Added missing return statements.

--- src/dal/BlogsMapper.php
+++ src/dal/BlogsMapper.php
@@ -11,21 +11,25 @@ class BlogsMapper extends Mapper implements IBlogsMapper
     public function fetchAll(): array
     {
         // TODO: Implement fetchAll() method.
+        return [];
     }
 
     public function fetch(int $id): ?Model
     {
         // TODO: Implement fetch() method.
+        return null;
     }
 
     public function insert(Model $model): ?Model
     {
         // TODO: Implement insert() method.
+        return null;
     }
 
     public function update(Model $model): ?Model
     {
         // TODO: Implement update() method.
+        return null;
     }
 
     public function delete(Model $model): void
--- src/dal/CommentsMapper.php
+++ src/dal/CommentsMapper.php
@@ -12,21 +12,25 @@ class CommentsMapper extends Mapper implements ICommentsMapper
     public function fetchAll(): array
     {
         // TODO: Implement fetchAll() method.
+        return [];
     }
 
     public function fetch(int $id): ?Model
     {
         // TODO: Implement fetch() method.
+        return null;
     }
 
     public function insert(Model $model): ?Model
     {
         // TODO: Implement insert() method.
+        return null;
     }
 
     public function update(Model $model): ?Model
     {
         // TODO: Implement update() method.
+        return null;
     }
 
     public function delete(Model $model): void
